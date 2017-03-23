2edba96548fa7c01242800ba9897f7d3eaee6a59
NonDesu <watashiwataidadesu@gmail.com>
2017-03-23 18:11:21 +0300

    Add files via upload

---
+++ Lab1-1.cpp
@@ -0,0 +1,58 @@
+#include <cstdlib>
+#include <iostream>
+#define K 10
+#define N 4
+#define M 4
+using namespace std;
+void INPUT(int *p, int n){
+    int i;
+    for(i=0;i<n;i++)
+        scanf("%d",(p+i));
+}
+void OUTPUT(int *p, int n, int m){
+    int i,j;
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++)
+            printf("%4d ",*(p+i*m+j));
+    printf("\n"); 
+    }   
+}
+int* MINDX(int *p, int n){
+    int i,*d=p,v=*p;
+    for(i=0;i<n;i++)
+        if(*(p+i)<v){
+            v=*(p+i);
+            d=p+i;
+        }
+    return d;    
+}
+void CHECK(int *p, int n){
+    int i,m=*p;
+    for(i=0;i<n;i++)
+        if(*(p+i)<m)m=*(p+i);
+    for(i=0;i<n;i++)
+        if(*(p+1)==m)throw std::exception();
+            
+}
+int main(){
+int a[K],b[N][M],i,temp,*ai,*bi;
+
+printf("A:\n");
+INPUT(&a[0],K);
+OUTPUT(&a[0],K,1);
+printf("B:\n");
+INPUT(&b[0][0],N*M);
+OUTPUT(&b[0][0],N,M);
+
+ai=MINDX(&a[0],K);
+bi=MINDX(&b[0][0],N*M);
+
+temp=*ai;
+*ai=*bi;
+*bi=temp;
+
+OUTPUT(&a[0],K,1);
+OUTPUT(&b[0][0],N,M);    
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
---
+++ Lab1-3.cpp
@@ -0,0 +1,71 @@
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
+#define NA 6 
+#define MA 9
+#define NB 6
+#define MB 7
+using namespace std;
+void INPUT(int **p, int n, int m){
+    int i,j;
+    srand(time(0));
+    for(i=0;i<n;i++){for(j=0;j<m;j++)
+        p[i][j]=rand()%10;}
+        //scanf("%d",(p+i));
+}
+void OUTPUT(int **p, int n, int m){
+    int i,j;
+    for(i=0;i<n;i++){
+        if(p[i]!=NULL){
+        for(j=0;j<m;j++)
+            printf("%4d ",p[i][j]);
+        printf("\n");
+        } 
+    }
+    printf("\n");   
+}
+void SHIFT(int **p, int n, int m, int x){
+int i,j;
+for(i=0;i<n;i++){
+    for(j=0;j<m;j++){
+        if(p[i][j]==x){
+            //delete [] p[i];
+            //free(p[i]);
+            p[i]=NULL;        
+        }
+    }    
+}
+}                     
+int main(){
+int **a,**b,i;
+a=(int **)malloc(NA*sizeof(int *));
+    for(i=0;i<NA;i++){
+        a[i]=(int *)malloc(MA*sizeof(int));
+    }
+b=(int **)malloc(NB*sizeof(int *));
+    for(i=0;i<NB;i++){
+        b[i]=(int *)malloc(MB*sizeof(int));
+    }
+
+INPUT(a,NA,MA);
+INPUT(b,NB,MB);
+OUTPUT(a,NA,MA);
+OUTPUT(b,NB,MB);
+SHIFT(a,NA,MA,1);
+SHIFT(b,NB,MB,0);
+OUTPUT(a,NA,MA);
+OUTPUT(b,NB,MB);
+for(int i=0;i<NA;i++){
+    if(a[i]!=NULL)
+    free(a[i]);
+}
+free(a);
+for(int i=0;i<NB;i++){
+    if(b[i]!=NULL)
+    free(b[i]);
+}
+free(b);   
+    
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
